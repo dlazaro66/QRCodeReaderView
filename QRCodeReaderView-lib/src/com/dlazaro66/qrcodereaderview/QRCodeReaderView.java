bd63abe1babafd672667ce328ef932be19fd1de2
David <dlazaro66@gmail.com>
2013-08-10 17:36:48 +0200

    Added license

--- QRCodeReaderView-lib/src/com/dlazaro66/qrcodereaderview/QRCodeReaderView.java
+++ QRCodeReaderView-lib/src/com/dlazaro66/qrcodereaderview/QRCodeReaderView.java
@@ -22,6 +22,30 @@ import com.google.zxing.client.android.camera.open.CameraManager;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
+/*
+*       Copyright 2013 David L‡zaro Esparcia.
+*
+*       This program is free software: you can redistribute it and/or
+modify
+*       it under the terms of the GNU General Public License as
+published by
+*       the Free Software Foundation, either version 3 of the License,
+or
+*       (at your option) any later version.
+*
+*       This program is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY; without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*       GNU General Public License for more details.
+*
+*       You should have received a copy of the GNU General Public
+License
+*       along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+
+
 /**
  * QRCodeReaderView - Class which uses ZXING lib and let you easily integrate a QR decoder view.
  * Take some classes and made some modifications in the original ZXING - Barcode Scanner project.  
