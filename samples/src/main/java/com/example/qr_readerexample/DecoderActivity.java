ce01a9a4e65fbb04dbfa8c128700aec219f2b0bb
Robert Zetzsche <zetzsche@movisens.com>
2016-11-03 14:47:24 +0100

    fixed force close of sample activity because of unknown camera ID -1

--- samples/src/main/java/com/example/qr_readerexample/DecoderActivity.java
+++ samples/src/main/java/com/example/qr_readerexample/DecoderActivity.java
@@ -111,6 +111,7 @@ public class DecoderActivity extends AppCompatActivity
 
     qrCodeReaderView.setAutofocusInterval(2000L);
     qrCodeReaderView.setOnQRCodeReadListener(this);
+    qrCodeReaderView.setBackCamera();
     flashlightCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
         qrCodeReaderView.setTorchEnabled(isChecked);
